2e171c1b4b8b7fdd8bf2f821504e82ffdc263e74
秋刀鱼的猫 <nieliquan@gmail.com>
2024-04-30 18:26:07 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,91 +1,64 @@
-import configparser
 import os
-import config as cf
-import process
-import privateCrypt
 
-config = configparser.ConfigParser()  # 类实例化
+'''
+*********** 商品配置 ***********
+'''
+ITEM_MAP = {
+    "10941": "53%vol 500ml贵州茅台酒（甲辰龙年）",
+    "10942": "53%vol 375ml×2贵州茅台酒（甲辰龙年）",
+    "10056": "53%vol 500ml茅台1935",
+    "2478": "53%vol 500ml贵州茅台酒（珍品）"
+}
 
+ITEM_CODES = ['10941']   # 需要预约的商品(默认只预约2个赚钱的茅子)
 
-def get_credentials_path():
-    if cf.CREDENTIALS_PATH is not None:
-        return cf.CREDENTIALS_PATH
-    else:
-        home_path = os.getcwd()
-        config_parent_path = os.path.join(home_path, 'myConfig')
-        config_path = os.path.join(config_parent_path, 'credentials')
-        if not os.path.exists(config_parent_path):
-            os.mkdir(config_parent_path)
-        return config_path
-
-
-path = get_credentials_path()
-# 这里config需要用encoding，以防跨平台乱码
-config.read(path, encoding="utf-8")
-sections = config.sections()
+'''
+*********** 消息推送配置 ***********
+push plus 微信推送,具体使用参考  https://www.pushplus.plus
+如没有配置则不推送消息
+为了安全,这里使用的环境配置.git里面请自行百度如何添加secrets.pycharm也可以自主添加.如果你实在不会,就直接用明文吧（O.o）
+'''
+PUSH_TOKEN = "fe585e83ba634d4e91b3ea5d28f69245"
+# PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
 
 
-def get_location():
-    while 1:
-        location = input(f"请输入精确小区位置，例如[小区名称]，为你自动预约附近的门店:").strip()
-        selects = process.select_geo(location)
+'''
+*********** 地图配置 ***********
+获取地点信息,这里用的高德api,需要自己去高德开发者平台申请自己的key
+'''
 
-        a = 0
-        for item in selects:
-            formatted_address = item['formatted_address']
-            province = item['province']
-            print(f'{a} : [地区:{province},位置:{formatted_address}]')
-            a += 1
-        user_select = input(f"请选择位置序号,重新输入请输入[-]:").strip()
-        if user_select == '-':
-            continue
-        select = selects[int(user_select)]
-        formatted_address = select['formatted_address']
-        province = select['province']
-        print(f'已选择 地区:{province},[{formatted_address}]附近的门店')
-        return select
+# AMAP_KEY = os.environ.get("GAODE_KEY")
+AMAP_KEY = "974303595175f91ba3ebff666b32ae98"
+
+'''
+*********** 个人账户认证配置 ***********
+个人用户 credentials 路径
+不配置,使用默认路径,在项目目录中;如果需要配置,你自己应该也会配置路径
+例如： CREDENTIALS_PATH = './myConfig/credentials'
+'''
+# CREDENTIALS_PATH = None
+CREDENTIALS_PATH = './myConfig/credentials'
 
 
-if __name__ == '__main__':
+'''
+*********** 个人加解密密钥 ***********
+为了解决credentials中手机号和token都暴露的问题,采用AES私钥加密,保障账号安全.
+这里采用ECB,没有采用CBC.如果是固定iv,那加一层也没多大意义;如果是不固定iv,那每次添加账号判重的时候都认为不一样,除非你每次再把配置全部反解密,去校验去重,得不偿失.
+key用了SHA-256转化,所以这里可以配置任意字符串,不用遵守AES算法要求密钥长度必须是16、24或32字节
+如果不会配置环境变量(建议学习)、不care安全性、非开源运行,你可以在这里明文指定,eg:PRIVATE_AES_KEY = '666666'
+ps:本来是写了判断是否配置密钥，可以自由选择明文保存的方式。但是还是为了安全性，限制了必须使用AES加密。哪怕是明文密钥。
+'''
+# PRIVATE_AES_KEY = os.environ.get("PRIVATE_AES_KEY")
 
-    aes_key = privateCrypt.get_aes_key()
+PRIVATE_AES_KEY = '666666'
 
-    while 1:
-        process.init_headers()
-        location_select: dict = get_location()
-        province = location_select['province']
-        city = location_select['city']
-        location: str = location_select['location']
 
-        mobile = input("输入手机号[13812341234]:").strip()
-        process.get_vcode(mobile)
-        code = input(f"输入 [{mobile}] 验证码[1234]:").strip()
-        token, userId = process.login(mobile, code)
-
-        endDate = input(f"输入 [{mobile}] 截止日期(必须是YYYYMMDD,20230819)，如果不设置截止，请输入9：").strip()
-
-        # 为了增加辨识度，这里做了隐私处理，不参与任何业务逻辑
-        hide_mobile = mobile.replace(mobile[3:7], '****')
-        # 因为加密了手机号和Userid，所以token就不做加密了
-        encrypt_mobile = privateCrypt.encrypt_aes_ecb(mobile, aes_key)
-        encrypt_userid = privateCrypt.encrypt_aes_ecb(str(userId), aes_key)
-
-        if encrypt_mobile not in sections:
-            config.add_section(encrypt_mobile)  # 首先添加一个新的section
-
-        config.set(encrypt_mobile, 'hidemobile', hide_mobile)
-        config.set(encrypt_mobile, 'enddate', endDate)
-        config.set(encrypt_mobile, 'userid', encrypt_userid)
-        config.set(encrypt_mobile, 'province', str(province))
-        config.set(encrypt_mobile, 'city', str(city))
-        config.set(encrypt_mobile, 'token', str(token))
-
-        config.set(encrypt_mobile, 'lat', location.split(',')[1])
-        config.set(encrypt_mobile, 'lng', location.split(',')[0])
-
-        config.write(open(path, 'w+', encoding="utf-8"))  # 保存数据
-
-        condition = input(f"是否继续添加账号[y/n]:").strip()
-
-        if condition.lower() == 'n':
-            break
+'''
+*********** 预约规则配置 ************
+因为目前支持代提的还是少,所以建议默认预约最近的门店
+'''
+_RULES = {
+    'MIN_DISTANCE': 0,   # 预约你的位置最近的门店
+    'MAX_SALES': 1,      # 预约本市出货量最大的门店
+}
+RESERVE_RULE = 0         # 在这里配置你的规则，只能选择其中一个
